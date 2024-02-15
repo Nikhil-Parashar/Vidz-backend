9c1d137fbd49d907702cb3e32a522276e0e85055
Nikhil-Parashar <nikhilonlineparashar@gmail.com>
2024-02-15 23:58:20 +0530

    Update file paths and remove commented code

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 import dotenv from "dotenv";
-// import connectToDB from "./db/dbConnect.js";
-// import app from "./app.js";
+import connectToDB from "./src/db/dbConnect.js";
+import app from "./src/app.js";
 
 dotenv.config({
     path: "./.env" // if giving prob try "./.env"
 });
 
-// connectToDB()
-//     .then(() => {
-//         app.listen(process.env.PORT, () => {
-//             console.log(`Server is listening on: ${process.env.PORT}`);
-//         });
-//     })
-//     .catch((error) => console.log("MONGODB connection failed!!!: ", error));
+connectToDB()
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is listening on: ${process.env.PORT}`);
+        });
+    })
+    .catch((error) => console.log("MONGODB connection failed!!!: ", error));
 
-import express from "express";
+// import express from "express";
 
-const app = express();
+// const app = express();
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server is listening on: ${process.env.PORT}`);
-});
+// app.listen(process.env.PORT, () => {
+//     console.log(`Server is listening on: ${process.env.PORT}`);
+// });
 
-app.get("/", (req, res) => {
-    res.send("hello");
-});
+// app.get("/", (req, res) => {
+//     res.send("hello");
+// });
 
-export default app;
+// export default app;
