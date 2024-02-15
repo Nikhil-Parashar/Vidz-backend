5ea3ffab59ee6c3c1fb90a7634eff17735771426
Nikhil-Parashar <nikhilonlineparashar@gmail.com>
2024-02-15 23:18:24 +0530

    Add initial server setup

---
+++ index.js
@@ -0,0 +1,29 @@
+import dotenv from "dotenv";
+// import connectToDB from "./db/dbConnect.js";
+// import app from "./app.js";
+
+dotenv.config({
+    path: "./.env" // if giving prob try "./.env"
+});
+
+// connectToDB()
+//     .then(() => {
+//         app.listen(process.env.PORT, () => {
+//             console.log(`Server is listening on: ${process.env.PORT}`);
+//         });
+//     })
+//     .catch((error) => console.log("MONGODB connection failed!!!: ", error));
+
+import express from "express";
+
+const app = express();
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is listening on: ${process.env.PORT}`);
+});
+
+app.get("/", (req, res) => {
+    res.send("hello");
+});
+
+export default app;
